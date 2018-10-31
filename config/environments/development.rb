21312957f3dad90d263e37d110096ec58d29845d
Chris Webb <webbc99@gmail.com>
2018-10-31 13:13:54 +0000

    updated development env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,9 +64,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('CMG_S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('CMG_AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('CMG_AWS_SECRET_ACCESS_KEY'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
       s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
     }
