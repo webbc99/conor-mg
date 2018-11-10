170e6f49707989c8f65c391b44bc66d052b1a84a
Aerodyne <webbc99@gmail.com>
2018-11-10 23:46:47 +0000

    Adjusted styling, introduced CV download and WIP portfolio index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,12 @@ class WelcomeController < ApplicationController
     @article = Article.last
     @user = current_user
   end
+
+  def download_cv
+    send_file(
+      "#{Rails.root}/public/puzzle_game_menu.png",
+      filename: "puzzle_game_menu.png",
+      type: "application/png"
+      )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'portfolio', to: 'portfolio#index'
+  get 'download_cv', to: 'welcome#download_cv'
 end
